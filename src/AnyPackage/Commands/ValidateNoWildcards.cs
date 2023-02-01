7a449dce664822b01cce50ffc0f4aeb76453413b
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2023-01-31 20:47:28 -0600

    Add ValidateNoWildcards attribute (#13)

---
+++ src/AnyPackage/Commands/ValidateNoWildcards.cs
@@ -0,0 +1,34 @@
+// Copyright (c) Thomas Nieto - All Rights Reserved
+// You may use, distribute and modify this code under the
+// terms of the MIT license.
+
+using System.Collections;
+using System.Linq;
+using System.Management.Automation;
+
+namespace AnyPackage.Commands
+{
+    public class ValidateNoWildcardsAttribute : ValidateArgumentsAttribute
+    {
+        protected override void Validate(object arguments, EngineIntrinsics engineIntrinsics)
+        {
+            var collection = arguments as IEnumerable;
+
+            if (collection is not null)
+            {
+                var values = collection
+                             .Cast<object>()
+                             .Select(x => x.ToString())
+                             .ToArray();
+
+                foreach (var value in values)
+                {
+                    if (WildcardPattern.ContainsWildcardCharacters(value))
+                    {
+                        throw new ValidationMetadataException("The parameter does not support wildcards.");
+                    }
+                }
+            }
+        }
+    }
+}
