2bf8780db2ee2cb17170c43ce75213b407b8c1be
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2023-03-11 18:51:14 -0600

    Stub out enum xml comments

--- src/AnyPackage/Provider/PackageVersionScheme.cs
+++ src/AnyPackage/Provider/PackageVersionScheme.cs
@@ -1,11 +1,33 @@
 namespace AnyPackage.Provider
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public enum PackageVersionScheme
     {
+        /// <summary>
+        /// 
+        /// </summary>
         AlphaNumeric,
+
+        /// <summary>
+        /// 
+        /// </summary>
         Integer,
+
+        /// <summary>
+        /// 
+        /// </summary>
         MultiPartNumeric,
+
+        /// <summary>
+        /// 
+        /// </summary>
         MultiPartNumericSuffix,
+
+        /// <summary>
+        /// 
+        /// </summary>
         SemanticVersion
     }
 }
