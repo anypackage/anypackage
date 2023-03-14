26287f9859b60fcb22fc6ed8709e78f29b450ab2
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2023-03-14 18:13:39 -0500

    Add constructor tests

--- test/Version.Tests.ps1
+++ test/Version.Tests.ps1
@@ -9,6 +9,195 @@ using namespace AnyPackage.Provider
 param()
 
 Describe PackageVersion {
+    Context 'Constructor' {
+        BeforeDiscovery {
+            $versions = @(
+                @{
+                    Version      = '1.2.3'
+                    Scheme       = 'SemanticVersion'
+                    Major        = 1
+                    Minor        = 2
+                    Patch        = 3
+                    Revision     = $null
+                    Parts        = @(1, 2, 3)
+                    Suffix       = $null
+                    IsPrerelease = $false
+                    Prerelease   = @()
+                    HasMetadata  = $false
+                    Metadata     = @()
+                }
+                @{
+                    Version      = '1.2.3-alpha'
+                    Scheme       = 'SemanticVersion'
+                    Major        = 1
+                    Minor        = 2
+                    Patch        = 3
+                    Revision     = $null
+                    Parts        = @(1, 2, 3)
+                    Suffix       = $null
+                    IsPrerelease = $true
+                    Prerelease   = @('alpha')
+                    HasMetadata  = $false
+                    Metadata     = @()
+                }
+                @{
+                    Version      = '1.2.3-alpha.1'
+                    Scheme       = 'SemanticVersion'
+                    Major        = 1
+                    Minor        = 2
+                    Patch        = 3
+                    Revision     = $null
+                    Parts        = @(1, 2, 3)
+                    Suffix       = $null
+                    IsPrerelease = $true
+                    Prerelease   = @('alpha', 1)
+                    HasMetadata  = $false
+                    Metadata     = @()
+                }
+                @{
+                    Version      = '1.2.3+build'
+                    Scheme       = 'SemanticVersion'
+                    Major        = 1
+                    Minor        = 2
+                    Patch        = 3
+                    Revision     = $null
+                    Parts        = @(1, 2, 3)
+                    Suffix       = $null
+                    IsPrerelease = $false
+                    Prerelease   = @()
+                    HasMetadata  = $true
+                    Metadata     = @('build')
+                }
+                @{
+                    Version      = '1.2.3+build.1'
+                    Scheme       = 'SemanticVersion'
+                    Major        = 1
+                    Minor        = 2
+                    Patch        = 3
+                    Revision     = $null
+                    Parts        = @(1, 2, 3)
+                    Suffix       = $null
+                    IsPrerelease = $false
+                    Prerelease   = @()
+                    HasMetadata  = $true
+                    Metadata     = @('build', 1)
+                }
+                @{
+                    Version      = '1.2.3-alpha.1+build.1'
+                    Scheme       = 'SemanticVersion'
+                    Major        = 1
+                    Minor        = 2
+                    Patch        = 3
+                    Revision     = $null
+                    Parts        = @(1, 2, 3)
+                    Suffix       = $null
+                    IsPrerelease = $true
+                    Prerelease   = @('alpha', 1)
+                    HasMetadata  = $true
+                    Metadata     = @('build', 1)
+                }
+                @{
+                    Version      = '1'
+                    Scheme       = 'Integer'
+                    Major        = 1
+                    Minor        = $null
+                    Patch        = $null
+                    Revision     = $null
+                    Parts        = @(1)
+                    Suffix       = $null
+                    IsPrerelease = $false
+                    Prerelease   = @()
+                    HasMetadata  = $false
+                    Metadata     = @()
+                }
+                @{
+                    Version      = '1.2.3a'
+                    Scheme       = 'MultiPartNumericSuffix'
+                    Major        = 1
+                    Minor        = 2
+                    Patch        = 3
+                    Revision     = $null
+                    Parts        = @(1, 2, 3)
+                    Suffix       = 'a'
+                    IsPrerelease = $false
+                    Prerelease   = @()
+                    HasMetadata  = $false
+                    Metadata     = @()
+                }
+                @{
+                    Version      = '1.2'
+                    Scheme       = 'MultiPartNumeric'
+                    Major        = 1
+                    Minor        = 2
+                    Patch        = $null
+                    Revision     = $null
+                    Parts        = @(1, 2)
+                    Suffix       = $null
+                    IsPrerelease = $false
+                    Prerelease   = @()
+                    HasMetadata  = $false
+                    Metadata     = @()
+                }
+                @{
+                    Version      = '1.2.3.4'
+                    Scheme       = 'MultiPartNumeric'
+                    Major        = 1
+                    Minor        = 2
+                    Patch        = 3
+                    Revision     = 4
+                    Parts        = @(1, 2, 3, 4)
+                    Suffix       = $null
+                    IsPrerelease = $false
+                    Prerelease   = @()
+                    HasMetadata  = $false
+                    Metadata     = @()
+                }
+                @{
+                    Version      = '1.2.3.4.5.6'
+                    Scheme       = 'MultiPartNumeric'
+                    Major        = 1
+                    Minor        = 2
+                    Patch        = 3
+                    Revision     = 4
+                    Parts        = @(1, 2, 3, 4, 5, 6)
+                    Suffix       = $null
+                    IsPrerelease = $false
+                    Prerelease   = @()
+                    HasMetadata  = $false
+                    Metadata     = @()
+                }
+                @{
+                    Version      = 'abc'
+                    Scheme       = 'AlphaNumeric'
+                    Major        = $null
+                    Minor        = $null
+                    Patch        = $null
+                    Revision     = $null
+                    Parts        = @()
+                    Suffix       = $null
+                    IsPrerelease = $false
+                    Prerelease   = @()
+                    HasMetadata  = $false
+                    Metadata     = @()
+                }
+            )
+        }
+
+        It 'should return correct properties for <Version> version' -ForEach $versions {
+            $v = [AnyPackage.Provider.PackageVersion]::new($Version)
+            $v.Scheme | Should -Be $Scheme
+            $v.Major | Should -Be $Major
+            $v.Minor | Should -Be $Minor
+            $v.Patch | Should -Be $Patch
+            $v.Parts | Should -Be $Parts
+            $v.Suffix | Should -Be $Suffix
+            $v.IsPrerelease | Should -Be $IsPrerelease
+            $v.HasMetadata | Should -Be $HasMetadata
+            $v.Prerelease | Should -Be $Prerelease
+            $v.Metadata | Should -Be $Metadata
+        }
+    }
+    
     Context 'ToVersion() method' {
         BeforeDiscovery {
             $messages = @{
@@ -30,12 +219,12 @@ Describe PackageVersion {
             $goodVersions = @('1.0', '1.2', '1.2.3', '1.2.3.4')
         }
 
-        It 'version <version> should error' -ForEach $badVersions {
+        It 'should error for <version> version' -ForEach $badVersions {
             { [AnyPackage.Provider.PackageVersion]::new($Version).ToVersion() } |
                 Should -Throw -ExpectedMessage $Message
         }
 
-        It 'version <_> should not error' -ForEach $goodVersions {
+        It 'should work for <_> version' -ForEach $goodVersions {
             { [AnyPackage.Provider.PackageVersion]::new($_).ToVersion() } |
                 Should -Not -Throw
         }
