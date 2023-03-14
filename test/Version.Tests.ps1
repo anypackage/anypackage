8e36b722db80bd66c41c9d76151276fe1bbfd984
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2023-03-14 00:11:37 -0500

    Add CompareTo tests

--- test/Version.Tests.ps1
+++ test/Version.Tests.ps1
@@ -40,4 +40,54 @@ Describe PackageVersion {
                 Should -Not -Throw
         }
     }
+
+    Context 'CompareTo() method' {
+        BeforeDiscovery {
+            $versions = @(
+                @{ Version = '1.0'; Other = $null; Result = 1 }
+                @{ Version = '1.0'; Other = '1.0'; Result = 0 }
+                @{ Version = '1.0'; Other = '2.0'; Result = -1 }
+                @{ Version = '2.0'; Other = '1.0'; Result = 1 }
+                @{ Version = '1.0'; Other = '1.1'; Result = -1 }
+                @{ Version = '1.1'; Other = '1.0'; Result = 1 }
+                @{ Version = '1.0.0'; Other = '2.0.0'; Result = -1 }
+                @{ Version = '2.0.0'; Other = '1.0.0'; Result = 1 }
+                @{ Version = '1.0.1'; Other = '2.0'; Result = -1 }
+                @{ Version = '2.0.0'; Other = '1.0.1'; Result = 1 }
+                @{ Version = '1.2.3.4.5'; Other = '1.2.3.4.5'; Result = 0 }
+                @{ Version = '1.2.3.4.5.1'; Other = '1.2.3.4.5'; Result = 1 }
+                @{ Version = '1.2.3.4.5'; Other = '1.2.3.4.5.1'; Result = -1 }
+                @{ Version = '1.0.0'; Other = '1.0.0-alpha'; Result = 1 }
+                @{ Version = '1.0.0-alpha'; Other = '1.0.0'; Result = -1 }
+                @{ Version = '1.0.0-alpha'; Other = '1.0.0-alpha'; Result = 0 }
+                @{ Version = '1'; Other = '1.0.0-alpha'; Result = 1 }
+                @{ Version = '1.0.0-alpha'; Other = '1'; Result = -1 }
+                @{ Version = '1.0.0-alpha.1'; Other = '1.0.0-alpha.1'; Result = 0 }
+                @{ Version = '1.0.0-alpha.1'; Other = '1.0.0-alpha.2'; Result = -1 }
+                @{ Version = '1.0.0-alpha.2'; Other = '1.0.0-alpha.1'; Result = 1 }
+                @{ Version = '1.0.0-alpha.1.1'; Other = '1.0.0-alpha.1'; Result = 1 }
+                @{ Version = '1.0.0-alpha.1'; Other = '1.0.0-alpha.1.1'; Result = -1 }
+                @{ Version = '1.0.0-alpha.1'; Other = '1.0.0-alpha.2'; Result = -1 }
+                @{ Version = '1.0a'; Other = '1.0a'; Result = 0 }
+                @{ Version = '1.0a'; Other = '1.0b'; Result = -1 }
+                @{ Version = '1.0b'; Other = '1.0a'; Result = 1 }
+                @{ Version = '1.0'; Other = '1.0a'; Result = -1 }
+                @{ Version = '1.0a'; Other = '1.0'; Result = 1 }
+                @{ Version = '1.0.0a'; Other = '1.0.0-alpha'; Result = 1 }
+                @{ Version = '1.0.0-alpha'; Other = '1.0.0a'; Result = -1 }
+                @{ Version = '1.0'; Other = 'abc'; Result = -1 }
+                @{ Version = 'abc'; Other = '1.0'; Result = 1 }
+                @{ Version = 'abc'; Other = 'abc'; Result = 0 }
+                @{ Version = 'abc'; Other = 'def'; Result = -1 }
+                @{ Version = 'def'; Other = 'abc'; Result = 1 }
+            )
+        }
+
+        It "should return '<Result>' for '<Version>' compare to '<Other>'" -ForEach $versions {
+            $a = [AnyPackage.Provider.PackageVersion]$Version
+            $b = [AnyPackage.Provider.PackageVersion]$Other
+            
+            $a.CompareTo($b) | Should -Be $Result
+        }
+    }
 }
