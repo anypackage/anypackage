39a34ea9812daed81594b1c31f3b139325b66654
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2024-08-25 19:30:49 -0500

    Add package and source PSObject constructor (#205)

---
+++ src/AnyPackage/Provider/Extensions.cs
@@ -0,0 +1,27 @@
+// Copyright (c) Thomas Nieto - All Rights Reserved
+// You may use, distribute and modify this code under the
+// terms of the MIT license.
+
+using System.Collections;
+
+namespace AnyPackage.Provider;
+
+internal static class Extensions
+{
+    internal static Dictionary<string, object?> ToDictionary(this Hashtable hashtable)
+    {
+        var dictionary = new Dictionary<string, object?>();
+
+        foreach (DictionaryEntry entry in hashtable)
+        {
+            var key = entry.Key.ToString();
+
+            if (key is not null)
+            {
+                dictionary.Add(key, entry.Value);
+            }
+        }
+
+        return dictionary;
+    }
+}
